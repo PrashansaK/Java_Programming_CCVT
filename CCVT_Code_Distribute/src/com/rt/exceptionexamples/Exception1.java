59964b205260af6909a3b338256b8546f31d8ca2
PrashansaK <prashansa.98@gmail.com>
2018-02-17 14:34:18 +0530

    Did the exception handling by adding a try catch block.

--- CCVT_Code_Distribute/src/com/rt/exceptionexamples/Exception1.java
+++ CCVT_Code_Distribute/src/com/rt/exceptionexamples/Exception1.java
@@ -2,7 +2,14 @@ package com.rt.exceptionexamples;
 
 class Exception1 {
 public static void main(String args[]) {
-int d = 0;
-int a = 42 / d;
+
+	try{
+	int d = 0;
+	int a = 42 / d;
+	}
+	catch(ArithmeticException e){
+		System.out.println("Division by zero exception!!");
+	}
+	//Changes made: Added try catch block to handle the exception
 }
 }
